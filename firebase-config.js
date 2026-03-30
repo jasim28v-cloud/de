bcfb640c460a6edc5fccdc74379d84ec2c514ebb
jasim28v-cloud <jasim28v@gmail.com>
2026-03-31 01:25:04 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,24 +1,28 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-import { getDatabase, ref, push, set, onValue, update, get, child } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+import { getDatabase, ref, push, set, onValue, update, get, child, remove, onChildAdded, onChildChanged, query, orderByChild, equalTo } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+import { getStorage, ref as storageRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-storage.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC8rvv7OihIJbTPz8wLNPPEURP6HeGXPos",
-    authDomain: "coco-989ec.firebaseapp.com",
-    databaseURL: "https://coco-989ec-default-rtdb.firebaseio.com",
-    projectId: "coco-989ec",
-    storageBucket: "coco-989ec.firebasestorage.app",
-    messagingSenderId: "476879537305",
-    appId: "1:476879537305:web:4bd41c433cb4a1efba6408"
+    apiKey: "AIzaSyAxtEkrEgl0C9djPkxKKX-sENtOzPEbHB8",
+    authDomain: "tope-e5350.firebaseapp.com",
+    databaseURL: "https://tope-e5350-default-rtdb.firebaseio.com",
+    projectId: "tope-e5350",
+    storageBucket: "tope-e5350.firebasestorage.app",
+    messagingSenderId: "187788115549",
+    appId: "1:187788115549:web:0f3c00ff62c1ebc5ed97b4",
+    measurementId: "G-YERBCEZEW9"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
+export const storage = getStorage(app);
 
-export { ref, push, set, onValue, update, get, child };
+export { ref, push, set, onValue, update, get, child, remove, onChildAdded, onChildChanged, query, orderByChild, equalTo, storageRef, uploadBytes, getDownloadURL };
 
-export const CLOUD_NAME = 'dnmpmysk6';
-export const UPLOAD_PRESET = 'do_2gg';
+export const CLOUD_NAME = 'daemk3hut';
+export const UPLOAD_PRESET = 'fok2_k';
+export const AGORA_APP_ID = '929646610d814d529a06c4081c81325f';
 
-console.log('✅ Firebase configured');
+console.log('✅ TGramE Complete Ready');
